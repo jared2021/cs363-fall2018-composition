9abc05ace734b64d1af12d9e147c580f48dd2c00
Jared Schoolcraft <jascho@tesla.cs.iupui.edu>
2018-09-25 12:41:44 -0400

    Pushing chances to my driver file.

--- driver.cpp
+++ driver.cpp
@@ -3,6 +3,25 @@
 
 int main()
 {
-	new Array <int> (5);
-	delete Array <int>;
+	Array <int> int_array(5,1);
+	//Array <int> int_array_two (5,1);
+	//int_array.resize(10);
+	//if(int_array==int_array_two)
+	//{
+		//std::cout<<"These arrays are equal.";
+	//}
+	//else
+	//{
+		//std::cout<<"These arrays are not equal.";
+	//}
+	//int_array_two.resize(10);
+	//if(int_array!=int_array_two)
+	//{
+		//std::cout<<"These arrays are not equal.";
+	//}
+	//else
+	//{
+		//std::cout<<"These arrays are equal";
+	//}
+	//return 0;	
 }
